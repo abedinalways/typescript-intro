a0943d354b6cf34d9165847620ab5266c5a734ef
abedinalways <nion.sk5067@gmail.com>
2025-01-04 17:20:46 +0600

    first typescript

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+console.log("Typescript is a modern javascript");
+var country = "Bangladesh";
+console.log(country);
---
+++ script.ts
@@ -0,0 +1,4 @@
+console.log("hello world");
+console.log("Typescript is a modern javascript");
+let country= "Bangladesh";
+console.log(country);
